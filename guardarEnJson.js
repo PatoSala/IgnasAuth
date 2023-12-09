7005d2e8fb329fc3fae4fc0c16abc3eb811ef84a
Nachitto2 <137418216+Nachitto2@users.noreply.github.com>
2023-12-09 16:05:37 -0300

    Add files via upload

---
+++ guardarEnJson.js
@@ -0,0 +1,29 @@
+const fs = require('fs');//import
+let usersDb = JSON.parse(fs.readFileSync('./usersDB.json'));//read
+
+function register(nombre,mail,contraseña){
+    objeto = {
+        username : nombre,
+        email : mail,
+        password : contraseña
+    }
+    usersDb.push(objeto);
+    let arrayJSON = JSON.stringify(usersDb);//convierte el array a cadena
+    fs.writeFileSync('./usersDB.json',arrayJSON);//escribe la cadena en el archivo 
+}
+
+function login(mail,contraseña){
+    let usersDb = JSON.parse(fs.readFileSync('./usersDB.json'));//read
+    for(let i = 0;i<usersDb.length;i++){
+        if (mail==usersDb[i].email && contraseña==usersDb[i].password){
+            console.log("Bem vindo", usersDb[i].username);
+            return 0;
+        }
+    }
+    console.log("Erro");
+}
+
+register("newUserName", "newUserEmail", "newUserPass");
+login("newUserEmail", "newUserPass");
+
+
